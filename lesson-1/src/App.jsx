641b2cfbe071acca0a03f5cd123e3544de55125c
Borushko Aleksandr <o_xx@list.ru>
2022-02-15 00:57:36 +0300

    lesson-1

---
+++ lesson-1/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.scss';
+import Message from './components/Message/Message';
+
+function App() {
+  return (
+    <div className='App'>
+      <Message message={'Привет, друг!'} />
+    </div>
+  );
+}
+
+export default App;
---
+++ lesson-1/src/components/Message/Message.jsx
@@ -0,0 +1,8 @@
+import './Message.scss';
+
+const Message = ({message}) => {
+  return (
+    <h1 className='message'>{message}</h1>
+  );
+}
+export default Message;
